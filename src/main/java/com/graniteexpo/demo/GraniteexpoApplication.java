6223e4b36a9e8ee16b35599ca409b38c7b0392f6
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2026-02-03 14:47:33 +0530

    initial commit

---
+++ src/main/java/com/graniteexpo/demo/GraniteexpoApplication.java
@@ -0,0 +1,13 @@
+package com.graniteexpo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraniteexpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraniteexpoApplication.class, args);//code
+	}
+
+}
---
+++ src/test/java/com/graniteexpo/demo/GraniteexpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graniteexpo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraniteexpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
