7d00fa64ab97bb666371ddaf1ebe2375834e7da8
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2026-02-03 15:10:11 +0530

    connected succesfully to supabase backend's working

--- src/main/java/com/graniteexpo/demo/GraniteexpoApplication.java
+++ src/main/java/com/graniteexpo/demo/GraniteexpoApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GraniteexpoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraniteexpoApplication.class, args);//code
+		SpringApplication.run(GraniteexpoApplication.class, args);//codeworks
+
 	}
 
 }
