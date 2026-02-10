b18adbf7b6fb5260a92c553e5b0574c165e77b77
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2026-02-10 06:34:46 +0530

    added block dtos

--- src/main/java/com/graniteexpo/demo/dtos/CreateBlockRequestDTO.java
+++ src/main/java/com/graniteexpo/demo/dtos/CreateBlockRequestDTO.java
@@ -1,9 +1,15 @@
 package com.graniteexpo.demo.dtos;
 import jakarta.validation.constraints.NotBlank;
 public class CreateBlockRequestDTO {
+
     @NotBlank
     private String blockCode;
+    public String getBlockCode() {
+        return blockCode;
+    }
+    public void setBlockCode(String blockCode) {
+        this.blockCode = blockCode;
 
-    public String getBlockCode() { return blockCode; }
-    public void setBlockCode(String blockCode) { this.blockCode = blockCode; }
+    }
+
 }
