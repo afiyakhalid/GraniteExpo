5f68d45b805251c99862ab23d3f33607b5db7402
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2026-02-10 06:20:32 +0530

    added blockrepositories

--- src/main/java/com/graniteexpo/demo/dtos/CreateBlockRequestDTO.java
+++ src/main/java/com/graniteexpo/demo/dtos/CreateBlockRequestDTO.java
@@ -1,4 +1,9 @@
 package com.graniteexpo.demo.dtos;
-
+import jakarta.validation.constraints.NotBlank;
 public class CreateBlockRequestDTO {
+    @NotBlank
+    private String blockCode;
+
+    public String getBlockCode() { return blockCode; }
+    public void setBlockCode(String blockCode) { this.blockCode = blockCode; }
 }
--- src/main/java/com/graniteexpo/demo/repositories/BlockRepo.java
+++ src/main/java/com/graniteexpo/demo/repositories/BlockRepo.java
@@ -1,4 +1,16 @@
 package com.graniteexpo.demo.repositories;
+import com.graniteexpo.demo.entities.Block;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
-public class BlockRepo {
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BlockRepo extends JpaRepository<Block, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select b from Block b where b.id=:id")
+    Optional<Block> findByIdForUpdate(@Param("id") UUID id);
+
 }
