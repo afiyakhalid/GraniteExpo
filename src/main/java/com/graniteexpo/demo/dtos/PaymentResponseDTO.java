10e4588b6de041db64bf740cfcfd6b4b57160e72
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2026-04-02 05:18:24 +0530

    added paymentresponse dto

--- src/main/java/com/graniteexpo/demo/dtos/PaymentResponseDTO.java
+++ src/main/java/com/graniteexpo/demo/dtos/PaymentResponseDTO.java
@@ -1,4 +1,39 @@
 package com.graniteexpo.demo.dtos;
+import com.graniteexpo.demo.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
 
 public class PaymentResponseDTO {
+    private UUID paymentId;
+    private BigDecimal amount;
+    private String provider;
+    private String providerReference;
+    private PaymentStatus status;
+    public PaymentResponseDTO(){}
+
+    public PaymentResponseDTO(UUID paymentId, BigDecimal amount, String provider, String providerReference, PaymentStatus status) {
+        this.paymentId = paymentId;
+        this.amount = amount;
+        this.provider = provider;
+        this.providerReference = providerReference;
+        this.status = status;
+    }
+
+    public UUID getPaymentId() { return paymentId; }
+    public void setPaymentId(UUID paymentId) { this.paymentId = paymentId; }
+
+    public BigDecimal getAmount() { return amount; }
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
+
+    public String getProvider() { return provider; }
+    public void setProvider(String provider) { this.provider = provider; }
+
+    public String getProviderReference() { return providerReference; }
+    public void setProviderReference(String providerReference) { this.providerReference = providerReference; }
+
+    public PaymentStatus getStatus() { return status; }
+    public void setStatus(PaymentStatus status) { this.status = status; }
+
 }
